9882ef0377337e8efd7629170de8dbae86f465fb
Victor Locomotiv (macbook) <victor@locomotiv.com.br>
2014-05-14 09:39:58 -0300

    Talk about NodeJS and Express.

--- js/scripts.js
+++ js/scripts.js
@@ -4640,7 +4640,7 @@ Reveal.addEventListener('ready', function() {
   'use strict';
 
   var footer = TEMPLATES.footer({
-    title: 'Talk template'
+    title: 'NodeJS and Express'
   });
 
   var sectionEls = document.querySelectorAll('section:not(.stack)');
